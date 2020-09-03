4058216c1c2a691d740441b75b9a336b701b5128
Abhinandan18001 <abhinandan18001@iiitd.ac.in>
2020-09-03 07:07:48 +0530

    Updates

---
+++ precache-manifest.404157b1992596e67528cfec7aeb6ca6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dea592e466ceb73c5f07fc6e3a97de4",
+    "url": "/Airprobe-Task/index.html"
+  },
+  {
+    "revision": "b8b0bd2b77ca16b61af4",
+    "url": "/Airprobe-Task/static/css/main.259184ef.chunk.css"
+  },
+  {
+    "revision": "77d9bced68bc623fd745",
+    "url": "/Airprobe-Task/static/js/2.04868a54.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Airprobe-Task/static/js/2.04868a54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b0bd2b77ca16b61af4",
+    "url": "/Airprobe-Task/static/js/main.d1c7e037.chunk.js"
+  },
+  {
+    "revision": "aba2feb612033e0f4245",
+    "url": "/Airprobe-Task/static/js/runtime-main.08189c57.js"
+  },
+  {
+    "revision": "45a9b37bec3c6e5d7e7d0c0e159ba81f",
+    "url": "/Airprobe-Task/static/media/logo.45a9b37b.PNG"
+  }
+]);
